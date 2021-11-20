160f15bd1cd1be7403c3fccc190d7ece4fc7fdf2
Carlos Saenz <carlos.saenz.g99@gmail.com>
2021-11-20 13:52:50 -0500

    chore: initial commit

---
+++ novaeBACKEND/src/main/java/com/novae/back/NovaeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.novae.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaeBackendApplication.class, args);
+	}
+
+}
---
+++ novaeBACKEND/src/test/java/com/novae/back/NovaeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novae.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
